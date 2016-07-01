7c937d22081ce60c7bae139ae795d7aadfd4c888
Mason Earl <masonc.earl@gmail.com>
2016-07-01 15:57:34 -0600

    Added photo
    Mason Earl

---
+++ StudentList-master/Sources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  StudentList
+//
+//  Created by Andrew Madsen on 7/1/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+}
+
